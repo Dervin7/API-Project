f7671ef48a50559de499226f50fc0ad072591386
David Ervin <daervin777@gmail.com>
2022-07-27 21:39:06 -0400

    Updated script and style files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseURL ="https://pokeapi.co/api/v2/pokemon/"
 const containerDiv = document.getElementById(`container`)
 const searchDiv = document.getElementById(`searchContainer`)
+const PokemonDiv = null;
 
 const getPokeResults = (e) => {
     e.preventDefault();
@@ -27,39 +28,44 @@ const getPokeResults = (e) => {
 
 function displayData(pokemon) {
     console.log(pokemon.species.name)
+    if(containerDiv.firstElementChild != null) {
+        containerDiv.removeChild(PokemonDiv)
+    } else {null}
+
     const PokemonDiv = document.createElement("div")
-    const PokemonName = document.createElement("h2")
+    const PokemonName = document.createElement("h4")
     PokemonName.textContent = pokemon.species.name;
     PokemonDiv.appendChild(PokemonName)
 
     const Sprites = document.createElement("img")
     Sprites.src = pokemon.sprites.front_default;
     PokemonDiv.appendChild(Sprites)
+    // Sprites.setAttribute("id", "Sprites")
 
     const HpStats = document.createElement("h3")
-    HpStats.textContent = pokemon.stats[0].base_stat
+    HpStats.textContent = `HP: ${pokemon.stats[0].base_stat}`
     PokemonDiv.appendChild(HpStats)
-
+    
     const AtkStats = document.createElement("h3")
-    AtkStats.textContent = pokemon.stats[1].base_stat
+    AtkStats.textContent = `Atk: ${pokemon.stats[1].base_stat}`
     PokemonDiv.appendChild(AtkStats)
-
+    
     const SpAtkStats = document.createElement("h3")
-    SpAtkStats.textContent = pokemon.stats[2].base_stat
+    SpAtkStats.textContent = `SpAtk: ${pokemon.stats[2].base_stat}`
     PokemonDiv.appendChild(SpAtkStats)
-
+    
     const DefStats = document.createElement("h3")
-    DefStats.textContent = pokemon.stats[3].base_stat
+    DefStats.textContent = `Def: ${pokemon.stats[3].base_stat}`
     PokemonDiv.appendChild(DefStats)
-
+    
     const SpDefStats = document.createElement("h3")
-    SpDefStats.textContent = pokemon.stats[4].base_stat
+    SpDefStats.textContent = `SpDef: ${pokemon.stats[4].base_stat}`
     PokemonDiv.appendChild(SpDefStats)
-
+    
     const SpdStats = document.createElement("h3")
-    SpdStats.textContent = pokemon.stats[5].base_stat
+    SpdStats.textContent = `Spd: ${pokemon.stats[5].base_stat}`
     PokemonDiv.appendChild(SpdStats)
-
+    
     const PokemonTypes = document.createElement("p")
         switch (pokemon.types.length) {
             case 2:
@@ -71,8 +77,8 @@ function displayData(pokemon) {
             default:
                 break;
         }
-
-        PokemonDiv.appendChild(PokemonTypes)
-
-        containerDiv.appendChild(PokemonDiv)
+    
+    
+    PokemonDiv.appendChild(PokemonTypes)
+    containerDiv.appendChild(PokemonDiv)
 }
